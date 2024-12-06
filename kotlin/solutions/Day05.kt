3b733db911e06a073ccf12e9a04760b5caac9577
stylub <stylub0@gmail.com>
2024-12-06 16:16:31 +0100

    futher work

--- kotlin/solutions/Day05.kt
+++ kotlin/solutions/Day05.kt
@@ -1,107 +1,105 @@
-package solutions
-
-import utils.println
-import utils.readInput
-import kotlin.math.abs
-
-fun main() {
-    fun parseInput(input: List<String>): Pair<MutableList<Pair<Int, Int>>, MutableList<List<Int>>> {
-        var nextPart = false
-        val  order = mutableListOf<Pair<Int, Int>>()
-        val queue = mutableListOf<List<Int>>()
-
-        input.forEach{line ->
-            if(line == "") nextPart = true;
-            else if(!nextPart){
-                order.addLast(line.split("\\|".toRegex()).map(String::toInt).let { (a, b) -> a to b })
-            }else{
-                queue.addLast(line.split(",").map { it.toInt() })
-            }
-        }
-
-        return order to queue
-    }
-
-    fun preProcess(order : MutableList<Pair<Int, Int>>): HashMap<Int, MutableSet<Int>> {
-        val cantBefore = HashMap<Int,MutableSet<Int>>()
-
-        order.forEach{pair ->
-            val a = pair.first
-            val b = pair.second
-
-            if (cantBefore.containsKey(b)) {
-                cantBefore[b]?.add(a)
-            } else {
-                cantBefore[b] = mutableSetOf(a)
-            }
-        }
-
-        return cantBefore
-    }
-
-
-    fun part1(input: List<String>): Int {
-        val parsed = parseInput(input)
-        val order = parsed.first
-        val update = parsed.second
-
-        var cantBefore = preProcess(order)
-
-        var solution = 0
-
-        update.forEach{list ->
-            var pass = true
-
-            for(i in list.indices){
-                for(j in 0..<i){
-                    if(cantBefore[list[j]]?.contains(list[i]) == true){
-                        pass = false
-                    }
-                }
-            }
-            if(pass) solution += list[(list.size-1) /2]
-        }
-
-        return solution;
-    }
-
-    fun part2(input: List<String>): Int {
-        val parsed = parseInput(input)
-        val order = parsed.first
-        val update = parsed.second
-
-        val cantBefore = preProcess(order)
-        val pageComparator = object : Comparator<Int> {
-            override fun compare(num1: Int, num2: Int): Int {
-                if(cantBefore[num1]?.contains(num2) == true){
-                    return 1
-                }
-                else
-                {
-                    return -1
-                }
-            }
-        }
-        var solution = 0
-        update.forEach { list ->
-            val listSorted = list.sortedWith(pageComparator)
-            var isOk = false
-            for (i in list.indices) {
-                if (list[i] != listSorted[i]) {
-                    isOk = true
-                    break;
-                }
-            }
-            if(isOk) solution += listSorted[(listSorted.size - 1) / 2]
-        }
-        return solution
-    }
-
-    val testInput = readInput("Day05_test")
-    check(part1(testInput) == 143)
-    check(part2(testInput) == 123)
-
-    val input = readInput("Day05")
-    part1(input).println()
-    part2(input).println()
-}
+//package solutions
+//
+//import utils.println
+//import utils.readInput
+//import kotlin.math.abs
+//
+//fun main() {
+//    fun parseInput(input: List<String>): Pair<MutableList<Pair<Int, Int>>, MutableList<List<Int>>> {
+//        var nextPart = false
+//        val  order = mutableListOf<Pair<Int, Int>>()
+//        val queue = mutableListOf<List<Int>>()
+//
+//        input.forEach{line ->
+//            if(line == "") nextPart = true;
+//            else if(!nextPart){
+//                order.addLast(line.split("\\|".toRegex()).map(String::toInt).let { (a, b) -> a to b })
+//            }else{
+//                queue.addLast(line.split(",").map { it.toInt() })
+//            }
+//        }
+//
+//        return order to queue
+//    }
+//
+//    fun preProcess(order : MutableList<Pair<Int, Int>>): HashMap<Int, MutableSet<Int>> {
+//        val cantBefore = HashMap<Int,MutableSet<Int>>()
+//
+//        order.forEach{pair ->
+//            val a = pair.first
+//            val b = pair.second
+//
+//            if (cantBefore.containsKey(b)) {
+//                cantBefore[b]?.add(a)
+//            } else {
+//                cantBefore[b] = mutableSetOf(a)
+//            }
+//        }
+//
+//        return cantBefore
+//    }
+//
+//
+//    fun part1(input: List<String>): Int {
+//        val parsed = parseInput(input)
+//        val order = parsed.first
+//        val update = parsed.second
+//
+//        var cantBefore = preProcess(order)
+//
+//        var solution = 0
+//
+//        update.forEach{list ->
+//            var pass = true
+//
+//            for(i in list.indices){
+//                for(j in 0..<i){
+//                    if(cantBefore[list[j]]?.contains(list[i]) == true){
+//                        pass = false
+//                    }
+//                }
+//            }
+//            if(pass) solution += list[(list.size-1) /2]
+//        }
+//
+//        return solution;
+//    }
+//
+//    fun part2(input: List<String>): Int {
+//        val parsed = parseInput(input)
+//        val order = parsed.first
+//        val update = parsed.second
+//
+//        val cantBefore = preProcess(order)
+//        val pageComparator = object : Comparator<Int> {
+//            override fun compare(num1: Int, num2: Int): Int {
+//                return if(cantBefore[num1]?.contains(num2) == true){
+//                    1
+//                } else {
+//                    -1
+//                }
+//            }
+//        }
+//        var solution = 0
+//        update.forEach { list ->
+//            val listSorted = list.sortedWith(pageComparator)
+//            var isOk = false
+//            for (i in list.indices) {
+//                if (list[i] != listSorted[i]) {
+//                    isOk = true
+//                    break;
+//                }
+//            }
+//            if(isOk) solution += listSorted[(listSorted.size - 1) / 2]
+//        }
+//        return solution
+//    }
+//
+//    val testInput = readInput("Day05_test")
+//    check(part1(testInput) == 143)
+//    check(part2(testInput) == 123)
+//
+//    val input = readInput("Day05")
+//    part1(input).println()
+//    part2(input).println()
+//}
--- kotlin/solutions/Day06.kt
+++ kotlin/solutions/Day06.kt
@@ -5,27 +5,66 @@ import utils.readInput
 import kotlin.math.abs
 
 fun main() {
-    fun parseInput(input: List<String>): Pair<List<Int>, List<Int>> {
-        val room = mutableListOf<List<Int>>()
+    fun parseInput(input: List<String>): Pair<Pair<Int, Int>, MutableList<MutableList<Int>>> {
+        val room  = mutableListOf<MutableList<Int>>()
         var start = 0 to 0
         for(line in input.indices){
-            val curr_line = mutableListOf<Int>()
+            val currLine = mutableListOf<Int>()
             for(row in input[line].indices){
                 when(input[line][row]){
-                    '#' -> curr_line.addLast(-1)
-                    '.' -> curr_line.addLast(0)
+                    '#' -> currLine.add(-1)
+                    '.' -> currLine.add(0)
                     '^' -> {
-                        curr_line.addLast(1)
+                        currLine.add(1)
                         start = line to row
                     }
-
                 }
             }
+            room.add(currLine)
         }
+        return start to room
     }
 
     fun part1(input: List<String>): Int {
-        return 1;
+        var (pos,room) = parseInput(input)
+        var (x,y) = pos
+        var i = 0
+
+        var solution = 0
+
+        val steps: List<Pair<Int, Int>> = listOf(
+            0 to -1,
+            1 to 0,
+            0 to 1,
+            -1 to 0
+        )
+
+        val m = input.size
+        val n = input[0].length
+        var (dX,dY) = steps[i]
+
+        while(x + dX in 0..<n && y + dY in 0..<m ){
+            dX = steps[i].first
+            dY = steps[i].second
+            when(room[x + dX][y + dY]){
+                1 ->{
+                    x += dX
+                    y += dY
+                }
+                0 ->{
+                    x += dX
+                    y += dY
+                    room[x][y] = 1
+                    solution +=1
+                }
+                -1 ->{
+                    i+=1
+                    i %= 4
+                }
+            }
+        }
+
+        return solution;
     }
 
     fun part2(input: List<String>): Int {
@@ -33,7 +72,7 @@ fun main() {
     }
 
     val testInput = readInput("Day06_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 41)
     check(part2(testInput) == 1)
 
 //    val input = readInput("Day06")
