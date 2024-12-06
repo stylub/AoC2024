dd8f2b707f6a0faaa3366e137095e83a4bce3213
stylub <47119994+stylub@users.noreply.github.com>
2024-12-06 18:53:04 +0100

    day06

--- kotlin/solutions/Day04.kt
+++ kotlin/solutions/Day04.kt
@@ -1,77 +1,77 @@
-//package solutions
-//
-//import utils.println
-//import utils.readInput
-//
-//fun main() {
-//    fun parseInput(input: List<String>): Pair<List<Int>, List<Int>> {
-//        return  input.map { line ->
-//            line.split("\\s+".toRegex()).map(String::toInt).let { (a, b) -> a to b }
-//        }.unzip()
-//    }
-//
-//    fun findXMAS(input: List<String>): Int {
-//        var found = 0
-//        input.forEach { word ->
-//            found += "XMAS".toRegex().findAll(word).map { it }.toList().size
-//            found += "SAMX".toRegex().findAll(word).map { it }.toList().size
-//        }
-//
-//        return found
-//    }
-//
-//
-//    fun part1(input: List<String>): Int {
-//        val horizontal = mutableListOf<String>()
-//        for (j in input.indices) {
-//            for (i in 0..input[0].length - 4) {
-//                horizontal.add("" + input[j][i] + input[j][i + 1] + input[j][i + 2] + input[j][i + 3])
-//            }
-//        }
-//
-//        val vertical = mutableListOf<String>()
-//        for (j in 0..input.size - 4) {
-//            for (i in 0..<input[0].length) {
-//                vertical.addLast("" + input[j][i] + input[j + 1][i] + input[j + 2][i] + input[j + 3][i])
-//            }
-//        }
-//        val diagonal = mutableListOf<String>()
-//        for (i in 0..input.size - 4) {
-//            for (j in 0..input[0].length - 4) {
-//
-//                val word = "${input[i][j]}${input[i + 1][j + 1]}${input[i + 2][j + 2]}${input[i + 3][j + 3]}"
-//                diagonal.addLast(word)
-//            }
-//        }
-//        for (i in 3..<input.size) {
-//            for (j in 0..<input[0].length - 3) {
-//
-//                val word = "${input[i][j]}${input[i - 1][j + 1]}${input[i - 2][j + 2]}${input[i - 3][j + 3]}"
-//                diagonal.Unresolved reference: addLastaddLast(word)
-//            }
-//        }
-//        return findXMAS(horizontal) + findXMAS(vertical) + findXMAS(diagonal)
-//    }
-//
-//    fun part2(input: List<String>): Int {
-//        var solution = 0
-//
-//        for (i in 1..<input.size - 1) {
-//            for (j in 1..<input[0].length - 1) {
-//                val w1 = "${input[i - 1][j - 1]}${input[i][j]}${input[i + 1][j + 1]}"
-//                val w2 = "${input[i + 1][j - 1]}${input[i][j]}${input[i - 1][j + 1]}"
-//                if( (w1 == "MAS" || w1 == "SAM") && (w2 == "MAS" || w2 == "SAM"))
-//                    solution+=1
-//            }
-//        }
-//        return  solution
-//    }
-//
-//    val testInput = readInput("Day04_test")
-//    check(part1(testInput) == 18)
-//    check(part2(testInput) == 9)
-//
-//    val input = readInput("Day04")
-//    part1(input).println()
-//    part2(input).println()
-//}
+package solutions
+
+import utils.println
+import utils.readInput
+
+fun main() {
+    fun parseInput(input: List<String>): Pair<List<Int>, List<Int>> {
+        return  input.map { line ->
+            line.split("\\s+".toRegex()).map(String::toInt).let { (a, b) -> a to b }
+        }.unzip()
+    }
+
+    fun findXMAS(input: List<String>): Int {
+        var found = 0
+        input.forEach { word ->
+            found += "XMAS".toRegex().findAll(word).map { it }.toList().size
+            found += "SAMX".toRegex().findAll(word).map { it }.toList().size
+        }
+
+        return found
+    }
+
+
+    fun part1(input: List<String>): Int {
+        val horizontal = mutableListOf<String>()
+        for (j in input.indices) {
+            for (i in 0..input[0].length - 4) {
+                horizontal.add("" + input[j][i] + input[j][i + 1] + input[j][i + 2] + input[j][i + 3])
+            }
+        }
+
+        val vertical = mutableListOf<String>()
+        for (j in 0..input.size - 4) {
+            for (i in 0..<input[0].length) {
+                vertical.addLast("" + input[j][i] + input[j + 1][i] + input[j + 2][i] + input[j + 3][i])
+            }
+        }
+        val diagonal = mutableListOf<String>()
+        for (i in 0..input.size - 4) {
+            for (j in 0..input[0].length - 4) {
+
+                val word = "${input[i][j]}${input[i + 1][j + 1]}${input[i + 2][j + 2]}${input[i + 3][j + 3]}"
+                diagonal.addLast(word)
+            }
+        }
+        for (i in 3..<input.size) {
+            for (j in 0..<input[0].length - 3) {
+
+                val word = "${input[i][j]}${input[i - 1][j + 1]}${input[i - 2][j + 2]}${input[i - 3][j + 3]}"
+                diagonal.addLast(word)
+            }
+        }
+        return findXMAS(horizontal) + findXMAS(vertical) + findXMAS(diagonal)
+    }
+
+    fun part2(input: List<String>): Int {
+        var solution = 0
+
+        for (i in 1..<input.size - 1) {
+            for (j in 1..<input[0].length - 1) {
+                val w1 = "${input[i - 1][j - 1]}${input[i][j]}${input[i + 1][j + 1]}"
+                val w2 = "${input[i + 1][j - 1]}${input[i][j]}${input[i - 1][j + 1]}"
+                if( (w1 == "MAS" || w1 == "SAM") && (w2 == "MAS" || w2 == "SAM"))
+                    solution+=1
+            }
+        }
+        return  solution
+    }
+
+    val testInput = readInput("Day04_test")
+    check(part1(testInput) == 18)
+    check(part2(testInput) == 9)
+
+    val input = readInput("Day04")
+    part1(input).println()
+    part2(input).println()
+}
--- kotlin/solutions/Day06.kt
+++ kotlin/solutions/Day06.kt
@@ -2,7 +2,6 @@ package solutions
 
 import utils.println
 import utils.readInput
-import kotlin.math.abs
 
 fun main() {
     fun parseInput(input: List<String>): Pair<Pair<Int, Int>, MutableList<MutableList<Int>>> {
@@ -15,7 +14,7 @@ fun main() {
                     '#' -> currLine.add(-1)
                     '.' -> currLine.add(0)
                     '^' -> {
-                        currLine.add(1)
+                        currLine.add(0)
                         start = line to row
                     }
                 }
@@ -25,13 +24,7 @@ fun main() {
         return start to room
     }
 
-    fun part1(input: List<String>): Int {
-        var (pos,room) = parseInput(input)
-        var (x,y) = pos
-        var i = 0
-
-        var solution = 0
-
+    fun simulate(pos :Pair<Int, Int>, room: MutableList<MutableList<Int>> ): Pair<Int, MutableList<MutableList<Int>>>{
         val steps: List<Pair<Int, Int>> = listOf(
             0 to -1,
             1 to 0,
@@ -39,43 +32,73 @@ fun main() {
             -1 to 0
         )
 
-        val m = input.size
-        val n = input[0].length
+        val positionDirection = HashMap<Pair<Int, Int>,MutableSet<Int>>()
+
+        var (y,x) = pos
+        var i = 0
+
+        var solution = 0
+        val m = room.size
+        val n = room[0].size
         var (dX,dY) = steps[i]
 
         while(x + dX in 0..<n && y + dY in 0..<m ){
-            dX = steps[i].first
-            dY = steps[i].second
-            when(room[x + dX][y + dY]){
-                1 ->{
-                    x += dX
-                    y += dY
-                }
-                0 ->{
-                    x += dX
-                    y += dY
-                    room[x][y] = 1
-                    solution +=1
-                }
-                -1 ->{
-                    i+=1
-                    i %= 4
+            while(room[y + dY][x + dX] == -1){
+                i = (i + 1)%4
+                dX = steps[i].first
+                dY = steps[i].second
+            }
+            if(room[y][x] == 0){
+                room[y][x] = 1
+                solution +=1
+            }else if(positionDirection.containsKey(y to x)
+                && positionDirection[y to x]?.contains(i) == true){
+                return -1 to room
+            }
+            if(positionDirection.containsKey(y to x)){
+                positionDirection[y to x]?.add(i)
+            }else{
+                positionDirection[y to x] = mutableSetOf(i)
+            }
+
+
+            x += dX
+            y += dY
+        }
+        room[y][x] = 1
+        return solution to room
+    }
+
+    fun part1(input: List<String>): Int {
+        val (pos,room) = parseInput(input)
+        val (solution, roomAfter) = simulate(pos,room)
+        return solution + 1
+    }
+
+    fun part2(input: List<String>): Int {
+        val (pos,room) = parseInput(input)
+        val org = room.map { it.toMutableList() }.toMutableList()
+        var loops = 0
+        for (i in org.indices) {
+            for (j in org[i].indices) {
+                if (org[i][j] == 0 && i to j != pos ) {
+                    val modifiedRoom = org.map { it.toMutableList() }.toMutableList()
+                    modifiedRoom[i][j] = -1
+                    if(simulate(pos,modifiedRoom).first == -1){
+                        loops +=1
+                    }
                 }
             }
         }
 
-        return solution;
-    }
-
-    fun part2(input: List<String>): Int {
-        return 1
+        return loops
     }
 
     val testInput = readInput("Day06_test")
     check(part1(testInput) == 41)
-    check(part2(testInput) == 1)
+    check(part2(testInput) == 6)
 
-//    val input = readInput("Day06")
-//    part1(input).println()
-//    part2(input).println()
+    val input = readInput("Day06")
+    part1(input).println()
+    part2(input).println()
 }
